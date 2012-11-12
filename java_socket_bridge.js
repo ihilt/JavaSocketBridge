3f425e8cfced84538ff8db497000fb99279f8113
Ian Hilt <ihilt@cbccts.org>
2012-11-12 15:24:01 -0500

    Update java_socket_bridge.js
    Signed-off-by: Ian Hilt <ihilt@cbccts.org>

--- java_socket_bridge.js
+++ java_socket_bridge.js
@@ -1,50 +1,73 @@
+/*global document: false */
 // Global variables
-var java_socket_bridge_ready_flag = false;
+var java_socket_bridge_ready_flag = false,
+	is_connected_flag;
+
+// Get the applet object
+function get_java_socket_bridge() {
+	"use strict";
+	return document.getElementById('JavaSocketBridge');
+}
+
+function is_connected (b) {
+	"use strict";
+	return is_connected_flag;
+}
+
+// Report an error
+function on_socket_error(message) {
+	"use strict";
+	console.log(message);
+}
 
 // Applet reports it is ready to use
-function java_socket_bridge_ready(){
+function java_socket_bridge_ready() {
+	"use strict";
 	java_socket_bridge_ready_flag = true;
 }
 
+function is_ready() {
+	"use strict";
+	return java_socket_bridge_ready_flag;
+}
+
 // Connect to a given url and port
-function socket_connect(url, port){
-	if(java_socket_bridge_ready_flag){
-		return get_java_socket_bridge().connect(url, port);
-	}
-	else{
+function socket_connect(url, port) {
+	"use strict";
+	if (java_socket_bridge_ready_flag) {
+		if (get_java_socket_bridge().connect(url, port) !== true) {
+			is_connected_flag = false;
+			throw new Error("Could not connect");
+		} else {
+			is_connected_flag = true;
+		}
+	} else {
 		on_socket_error("Java Socket Bridge cannot connect until the applet has loaded");
 	}
 }
 
 // Disconnect
-function socket_disconnect(){
-	if(java_socket_bridge_ready_flag){
-		return get_java_socket_bridge().disconnect();
-	}
-	else{
+function socket_disconnect() {
+	"use strict";
+	if (java_socket_bridge_ready_flag) {
+		if (get_java_socket_bridge().disconnect() !== true) {
+			throw new Error("Could not disconnect");
+		}
+	} else {
 		on_socket_error("Java Socket Bridge cannot disconnect until the applet has loaded");
 	}
 }
 
 // Write something to the socket
-function socket_send(message){
-	if(java_socket_bridge_ready_flag){
+function socket_send(message) {
+	"use strict";
+	if (java_socket_bridge_ready_flag) {
 		return get_java_socket_bridge().send(message);
-	}
-	else{
+	} else {
 		on_socket_error("Java Socket Bridge cannot send a message until the applet has loaded");
 	}
 }
 
+// MUST BE IMPLEMENTED
 // Get something from the socket
-function on_socket_get(message){}
-
-// Report an error
-function on_socket_error(message){
-	alert(message);
-}
-
-// Get the applet object
-function get_java_socket_bridge(){
-	return document.getElementById('JavaSocketBridge');
-}
+//function on_socket_get(message) {"use strict"; }

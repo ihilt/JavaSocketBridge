fd25b59b568d4861b59efbab83a634d8a6084094
Ian Hilt <ihilt@cbccts.org>
2012-11-12 15:54:29 -0500

    Add a shell make script
    Signed-off-by: Ian Hilt <ihilt@cbccts.org>

---
+++ Source/make.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+javac -classpath "c:\program files\java\jdk1.6.0_31\jre\lib\plugin.jar" "JavaSocketBridge.java" "Listener.java"
+jar cvf JavaSocketBridge.jar "JavaSocketBridge.class" "Listener.class"
+jarsigner JavaSocketBridge.jar myNewKey

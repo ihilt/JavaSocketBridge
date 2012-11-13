703e439328aee484c740fbb9e9b0ee28116f68f4
Ian Hilt <ihilt@cbccts.org>
2012-11-13 15:28:53 -0500

    Move jar to parent directory
    Signed-off-by: Ian Hilt <ihilt@cbccts.org>

--- Source/make.sh
+++ Source/make.sh
@@ -2,3 +2,4 @@
 javac -classpath "c:\program files\java\jdk1.6.0_31\jre\lib\plugin.jar" "JavaSocketBridge.java" "Listener.java"
 jar cvf JavaSocketBridge.jar "JavaSocketBridge.class" "Listener.class"
 jarsigner JavaSocketBridge.jar myNewKey
+mv JavaSocketBridge.jar ..
